c632afb6c6065cf4f46b1b1e638457e2d164821a
nect2r <nect2r@kakao.com>
2021-12-19 17:28:55 +0900

    스프링 부트 실전 - 웹 어플리케이션 개발 환경셋팅 버전

---
+++ src/main/java/jpabook/jpashop/Member.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String username;
+}
